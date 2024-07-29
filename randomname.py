ec696708c1d0054c2a86677150a05bdf53ec4e8e
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2024-07-29 17:21:48 -0400

    updated code for characters

--- randomname.py
+++ randomname.py
@@ -1,13 +1,15 @@
 import random
+import string
 
-# Ask how many EC2 instances do the users want names for
-how_many = int(input('How many EC2 names would you like generated? '))
-
-# Ask for user input department name v 
-user_input = input('What department are you in? ')
+# Ask how many EC2 instances do the users want names for then ask for user input department name 
+how_many = int(input('Enter how many EC2 names you\'d like generated? '))
+department_name = input('What department are you in? ')
 
 # Generate random characters and numbers to go with the department name
 for a in range(how_many) :
+     random_char = ''.join(random.choices(string.ascii_letters, k=2))
+     random_num = ''.join(random.choices(string.digits, k=2))
+     new_names = department_name + '-' + random_char + str(random_num)
   
-# Output the amount of EC2 instant names, Should list them off
-     print(user_input + str(random.randrange(0,200)))
+# Output the amount of EC2 instances names
+     print(new_names)
