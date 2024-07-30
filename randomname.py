bb39a791c3005bedf20c181a465ba618cfe31cef
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2024-07-30 08:16:21 -0400

    nn

--- randomname.py
+++ randomname.py
@@ -1,11 +1,12 @@
 import random
+import string
 
 # Ask how many EC2 instances do the users want names for then ask for user input department name 
 how_many = int(input('Enter how many EC2 names you\'d like generated: '))
 department_name = input('What department are you in? ')
 
 # Generate random characters and numbers to go with the department name
-for a in range(how_many) :
+for i in range(how_many) :
      random_char = ''.join(random.choices(string.ascii_letters, k=2))
      new_names = department_name + '-' + random_char + str(random.randrange(0,200))
      
